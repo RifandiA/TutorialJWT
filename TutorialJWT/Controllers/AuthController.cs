96d312cfe0d6a164d62e9baed89072ea28d3c812
RifandiA <rifandialmanda@gmail.com>
2022-10-09 23:40:13 +0700

    Add project files.

---
+++ TutorialJWT/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutorialJWT.Interfaces;
+using TutorialJWT.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TutorialJWT.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticationManager jWTAuthenticationManager;
+
+        public AuthController(IAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+        }
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jWTAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
---
+++ TutorialJWT/Controllers/DataController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TutorialJWT.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TutorialJWT.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly IAuthenticationManager jWTAuthenticationManager;
+
+        public DataController(IAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+        }
+
+
+        // GET: api/<DataController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var claimIdentity = User.Identity;
+            var claimPricipal = new ClaimsPrincipal(claimIdentity);
+            var claim = claimPricipal.Claims.Select(c => new { c.Value }).ToList();
+            string username = claim[0].Value.ToString();
+            return new string[] { username, "Almand", "Teknologi" };
+        }
+
+        // GET api/<DataController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+    }
+}
