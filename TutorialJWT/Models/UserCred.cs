bf275e9ea9c65a348818bda271a150fbaea9ec84
rifandi <you@example.com>
2024-12-13 23:14:26 +0700

    --KodeRole

--- TutorialJWT/Models/UserCred.cs
+++ TutorialJWT/Models/UserCred.cs
@@ -9,5 +9,6 @@ namespace TutorialJWT.Models
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string KodeRole { get; set; }
     }
 }
